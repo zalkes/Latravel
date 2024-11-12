9fc5da71a2d67637efc19c12c8815a62df013bbd
Muhammad Afrizal Kesuma <140897214+zalkes@users.noreply.github.com>
2024-11-12 11:04:55 +0800

    nambah db + login/regis

---
+++ login_page/forgotpw.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+require "../database/koneksi.php";
+
+if (isset($_POST["submit"])){
+    $email = $_POST["email"];
+
+    $sql_select = mysqli_query($conn, "SELECT * FROM pengguna");
+
+    $count = 0;
+    while ($row = mysqli_fetch_assoc($sql_select)){
+        $pengguna[] = $row;
+        if ($pengguna[$count]["email"] == $email){
+            $_SESSION["email"] = $pengguna[$count]["email"];
+            echo "
+                <script>
+                    document.location.href = 'newpw.php';
+                </script>
+            ";
+            exit;
+        }
+        $count ++;
+    }
+
+    echo "
+        <script>
+            alert('Email tidak ditemukan');
+            document.location.href = 'forgotpw.php';
+        </script>
+    ";
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lupa Password</title>
+    <link rel="stylesheet" href="../elements/styles/login.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title-text">
+            <div class="title signup">
+                GANTI <br>PASSWORD
+            </div>
+        </div>
+        <div class="title-text">
+            <div id ="sub" class="title signup">
+                KONFIRMASI EMAIL
+            </div>
+        </div>
+        <div class="form-container">
+            <div class="form-inner">
+                <form action="" method="POST" class="signup">
+                    <div class="field">
+                        <input type="email" name="email" id="email"  placeholder="Email" required>
+                    </div>
+                    <br><br>
+                    <div class="field btn">
+                        <div class="btn-layer"></div>
+                        <input type="submit" name="submit" value="Kirim">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>  
+    <img src="../assets/images/logo.png" class="logo">
+</body>
+</html>
---
+++ login_page/newpw.php
@@ -0,0 +1,78 @@
+<?php 
+    session_start();
+    require "../database/koneksi.php";
+    if (isset($_POST["submit"])){
+        $password_new = $_POST["password_new"];
+        $conf_password_new = $_POST["conf_password_new"];
+        if ($password_new == $conf_password_new){
+            $email = $_SESSION["email"];
+            $password = password_hash($password_new, PASSWORD_DEFAULT);
+            $sql_update = mysqli_query($conn, "UPDATE pengguna SET pw = '$password' WHERE email = '$email'");
+            if ($sql_update){
+                echo "
+                    <script>
+                        alert('Password berhasil diubah');
+                        document.location.href = 'login.php';
+                    </script>
+                ";
+                exit;
+            } else {
+                echo "
+                    <script>
+                        alert('Password gagal diubah');
+                        document.location.href = 'newpw.php';
+                    </script>
+                ";
+                exit;
+            }
+        } else {
+            echo "
+                <script>
+                    alert('Pastikan konfirmasi password sudah benar');
+                    document.location.href = 'newpw.php';
+                </script>
+            ";
+            exit;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lupa Password</title>
+    <link rel="stylesheet" href="../elements/styles/login.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title-text">
+            <div class="title signup">
+                GANTI <br>PASSWORD
+            </div>
+        </div>
+        <div class="title-text">
+            <div id ="sub" class="title signup">
+                MASUKKAN PASSWORD BARU
+            </div>
+        </div>
+        <div class="form-container">
+            <div class="form-inner">
+                <form action="" method="POST" class="signup">
+                        <div class="field">
+                            <input type="password" name="password_new" id="password_new" placeholder="Password Baru" required>
+                        </div>
+                        <div class="field">
+                            <input type="password" name="conf_password_new" id="conf_password_new" placeholder="Konfirmasi Password" required>
+                        </div>
+                    <br><br>
+                    <div class="field btn">
+                        <div class="btn-layer"></div>
+                        <input type="submit" name="submit" value="Kirim">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>  
+    <img src="../assets/images/logo.png" class="logo">
+</body>
+</html>
